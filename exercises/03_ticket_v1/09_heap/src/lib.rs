d36ff3ca6340c7147b41e964d690383cc000c8eb
PinelliaC <wylbzc4928@gmail.com>
2025-02-10 17:59:57 +0800

    feat: exercises 03

--- exercises/03_ticket_v1/09_heap/src/lib.rs
+++ exercises/03_ticket_v1/09_heap/src/lib.rs
@@ -13,7 +13,15 @@ mod tests {
 
     #[test]
     fn string_size() {
-        assert_eq!(size_of::<String>(), todo!());
+        // 8 bytes for the pointer to the heap-allocated data
+        // 8 bytes for the length
+        // 8 bytes for the capacity
+        // -------------------------
+        // 24 bytes total
+        // -------------------------
+        // This is the size of the `String` struct itself, not the size of the data it points to.
+        // The data itself is heap-allocated and can be of any size.
+        assert_eq!(size_of::<String>(), 24);
     }
 
     #[test]
@@ -23,6 +31,6 @@ mod tests {
         // but, in general, the memory layout of structs is a more complex topic.
         // If you're curious, check out the "Type layout" section of The Rust Reference
         // https://doc.rust-lang.org/reference/type-layout.html for more information.
-        assert_eq!(size_of::<Ticket>(), todo!());
+        assert_eq!(size_of::<Ticket>(), 72);
     }
 }
