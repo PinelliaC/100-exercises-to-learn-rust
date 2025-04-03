04f20efdce7656c6c98f0292ed22bed5bf50445c
PinelliaC <wylbzc4928@gmail.com>
2025-04-03 17:23:53 +0800

    feat: exercise 05

--- exercises/05_ticket_v2/13_try_from/src/lib.rs
+++ exercises/05_ticket_v2/13_try_from/src/lib.rs
@@ -8,6 +8,33 @@ enum Status {
     Done,
 }
 
+#[derive(Debug, thiserror::Error)]
+enum StatusError {
+    #[error("Invalid status")]
+    InvalidStatus,
+}
+
+impl TryFrom<String> for Status {
+    type Error = StatusError;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        value.as_str().try_into()
+    }
+}
+
+impl TryFrom<&str> for Status {
+    type Error = StatusError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "todo" => Ok(Status::ToDo),
+            "inprogress" => Ok(Status::InProgress),
+            "done" => Ok(Status::Done),
+            _ => Err(StatusError::InvalidStatus),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
